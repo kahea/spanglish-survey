edcb31303be3ba6fc2b24cbf2ca9fc3f3fd8081b
Kahea <kaheah@gmail.com>
2015-05-19 00:57:40 +0000

    delete answers with question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
     has_attached_file :pic
     validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
     has_many :answers
+    before_destroy :purge
 
     def prev
         Question.find_by_order(self.order - 1)
@@ -9,6 +10,10 @@ class Question < ActiveRecord::Base
 
     def next
         Question.find_by_order(self.order + 1)
+    end
+
+    def purge
+        self.answers.all.destroy_all
     end
 
 end
